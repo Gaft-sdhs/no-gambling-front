a39c0f6067ebaed6f606a68cbf91e733b2a3f34f
paulsea27115 <csb070115@gmail.com>
2024-06-12 12:09:08 +0900

    헤더 완성,배너 제작중

---
+++ src/components/header/Banners.jsx
@@ -0,0 +1,10 @@
+import "./css/banners.css"
+const Banners = ()=>{
+  return (
+    <section className="banners">
+        
+    </section>
+  )
+}
+
+export default Banners;
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,8 @@
+const Logo = ()=>{
+  return (
+    <a>
+      <img src="/pageLogo.png" alt="logo" />
+    </a>
+  )
+}
+export default Logo;
