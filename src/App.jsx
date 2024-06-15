33cde905c57eba166e65550aa9a701a3d5dccfd4
paulsea27115 <csb070115@gmail.com>
2024-06-15 18:56:25 +0900

    홀짝 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Header from "./components/header/Header";
 import Index from "./pages/Index";
 import Snail from "./pages/Snail";
-import Ladder from "./pages/Ladder";
-import Sniffling from "./pages/Sniffling"
 import Store from "./pages/Store";
+import Ladder from "./pages/Ladder";
 import Script from "./Script";
 import { Route,Routes } from "react-router-dom";
 
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />}/>
         <Route path="/snail" element={<Snail />}/>
-        <Route path="/Sniffling" element={<Sniffling />}/>
         <Route path="/Ladder" element={<Ladder />}/>
         <Route path="/Store" element={<Store />}/>
       </Routes>
