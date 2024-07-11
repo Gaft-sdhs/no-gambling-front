dda4b1f193caf0d1629b5837bfc3aabb92d9e93d
paulsea27115 <csb070115@gmail.com>
2024-07-11 14:41:17 +0900

    [feat] 목록 아이콘

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import Logo from "./Logo";
 import HeaderItem from "./HeaderItem";
 import Banners from "./Banners";
+import MenuBtn from "./MenuBtn";
 import "./css/header.css";
 const Header = () => {
   return (
@@ -9,12 +10,13 @@ const Header = () => {
         <div className="logo">
           <Logo />
         </div>
-        <div className="nav">
-          <HeaderItem name={"달팽이"} link={"/snail"}/>
-          <HeaderItem name={"사다리"} link={"/ladder"}/>
-          <HeaderItem name={"파워볼"} link={"/Powerball"}/>
-          <HeaderItem name={"충전"} link={""}/>
-        </div>
+        <ul className="nav">
+          <HeaderItem name={"달팽이"} link={"/snail"} />
+          <HeaderItem name={"사다리"} link={"/ladder"} />
+          <HeaderItem name={"파워볼"} link={"/Powerball"} />
+          <HeaderItem name={"충전"} link={""} />
+        </ul>
+        <MenuBtn />
       </header>
       <Banners />
     </>
--- src/components/header/HeaderItem.jsx
+++ src/components/header/HeaderItem.jsx
@@ -2,10 +2,10 @@ import "./css/headerItem.css";
 import { Link } from "react-router-dom";
 const HeaderItem = ({ name, link }) => {
   return (
-    <div className="headerItem">
+    <li className="headerItem">
       <Link to={link}>{name}</Link>
       <span className="underBar"></span>
-    </div>
+    </li>
   );
 };
 export default HeaderItem;
