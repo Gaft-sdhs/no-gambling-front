cdd8557ec9eb02677e02497f5732a434e3a1c005
rlagusals1102 <hmkim112@naver.com>
2024-07-12 13:39:23 +0900

    [feat] service

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,7 @@ self.addEventListener('install', event => {
         './index.html',
         './manifest.webmanifest',
         './android/android-launchericon-512-512.png',
+        './offline.html', // 오프라인 페이지 추가
         // 다른 필요한 파일들
       ]);
     })
@@ -31,57 +32,22 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request).then(networkResponse => {
-        return caches.open('static-cache').then(cache => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match('offline.html');
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        return cachedResponse || fetch(event.request).then(networkResponse => {
+          return caches.open('static-cache').then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
         });
-      });
-    })
-  );
-});
-
-// Push Notifications
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: 'icons/icon-512x512.png',
-    badge: 'icons/badge-128x128.png',
-  };
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
-});
-
-// Periodic Sync
-self.addEventListener('periodicsync', event => {
-  if (event.tag === 'sync-tag') {
-    event.waitUntil(doSomePeriodicSync());
+      })
+    );
   }
 });
-
-// Background Sync
-self.addEventListener('sync', event => {
-  if (event.tag === 'sync-tag') {
-    event.waitUntil(doSomeBackgroundSync());
-  }
-});
-
-function doSomePeriodicSync() {
-  return fetch('/sync-endpoint').then(response => {
-    return response.json();
-  }).then(data => {
-    console.log('Periodic Sync data: ', data);
-  });
-}
-
-function doSomeBackgroundSync() {
-  return fetch('/sync-endpoint').then(response => {
-    return response.json();
-  }).then(data => {
-    console.log('Background Sync data: ', data);
-  });
-}
