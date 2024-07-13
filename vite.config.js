0daa9e89b94426b3d669cb0fe38f5dbee8f01c59
rlagusals1102 <rlagusals1102@gmail.com>
2024-07-13 16:05:29 +0900

    [feat] PWA 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,37 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()], server: {
-        port: 3000,
-    }, build: {
-        outDir: 'dist',
-    },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['robots.txt'],
+      manifest: {
+        name: 'yongsanspo',
+        short_name: 'jamondan',
+        description: '용산 경찰서 자문단',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
