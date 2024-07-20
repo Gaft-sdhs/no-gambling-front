b765367141375c9a81d186186143a3fc10618f3f
rlagusals1102 <rlagusals1102@gmail.com>
2024-07-20 15:47:47 +0900

    [feat] webpack 적용

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,13 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    proxy: {
+      '/webpack': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/webpack/, '')
+      }
+    }
   },
   build: {
     outDir: 'dist',
