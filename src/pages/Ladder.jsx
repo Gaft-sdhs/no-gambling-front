fbb5a89dc10dda0eea7f9274823e39e86b71d67c
paulsea27115 <csb070115@gmail.com>
2024-07-26 17:09:53 +0900

    [feat] 모바일 화면에서 페이지 이동 시 nav 창 안사라지는 부분 수정

--- src/pages/Ladder.jsx
+++ src/pages/Ladder.jsx
@@ -4,17 +4,21 @@ import React from "react";
 import "./css/ladder.css";
 import LadderGame from "../components/ladder/LadderGame";
 import Ranking from "../components/ranking/Ranking";
+import Header from "../components/header/Header";
 
 // Ladder 컴포넌트: 사다리 게임과 랭킹 컴포넌트를 렌더링
 const Ladder = () => {
-    return (
-        <main className="ladder">
-            <section className="container">
-                <LadderGame />
-                <Ranking />
-            </section>
-        </main>
-    );
-}
+  return (
+    <>
+      <Header />
+      <main className="ladder">
+        <section className="container">
+          <LadderGame />
+          <Ranking />
+        </section>
+      </main>
+    </>
+  );
+};
 
 export default Ladder;
