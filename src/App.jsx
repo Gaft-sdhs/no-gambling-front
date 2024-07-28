baa69c6c17be193d6555332a06eda4d0496b16e7
paulsea27115 <csb070115@gmail.com>
2024-07-28 17:32:01 +0900

    [feat] 처음 랜딩 시 나오는 모달창 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Snail from "./pages/Snail";
@@ -7,25 +7,11 @@ import PowerBall from "./pages/PowerBall";
 import LossWarning from "./components/LossWarning/LossWarning"; // LossWarning 컴포넌트 추가
 import "./css/index.css";
 
+export const LostCountContext = createContext();
+
 function App() {
   const [showModal, setShowModal] = useState(false);
   const [lostCount, setLostCount] = useState(0); // 잃은 횟수 카운트
-
-  useEffect(() => {
-    const checkScreenSize = () => {
-      if (window.innerWidth < 30000) {
-        setShowModal(true);
-      } else {
-        setShowModal(false);
-      }
-    };
-
-    checkScreenSize();
-    window.addEventListener("resize", checkScreenSize);
-    return () => {
-      window.removeEventListener("resize", checkScreenSize);
-    };
-  }, []);
 
   // 로컬 스토리지에서 사용자 재산 데이터를 가져오기
   useEffect(() => {
@@ -41,7 +27,7 @@ function App() {
 
   // 3번 잃을 때마다 모달을 띄우기
   useEffect(() => {
-    if (lostCount > 0 && lostCount % 3 === 0) {
+    if (lostCount > 0 && lostCount % 2 === 0) {
       setShowModal(true);
     }
   }, [lostCount]);
@@ -49,13 +35,15 @@ function App() {
   return (
     <>
       {showModal && <LossWarning onClose={handleCloseModal} />}
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/snail" element={<Snail />} />
-        <Route path="/ladder" element={<Ladder />} />
-        <Route path="/powerball" element={<PowerBall />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <LostCountContext.Provider value={(lostCount, setLostCount)}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/snail" element={<Snail />} />
+          <Route path="/ladder" element={<Ladder />} />
+          <Route path="/powerball" element={<PowerBall />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </LostCountContext.Provider>
     </>
   );
 }
