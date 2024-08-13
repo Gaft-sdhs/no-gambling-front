f3b39ad18ab0f7d1c00407a612205c8326e1ee4c
rlagusals1102 <hmkim112@naver.com>
2024-08-13 13:36:14 +0900

    [feat] 개발 환경 콘솔 출력

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
-export default {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['transform-remove-console'],
-};
+export default function(api) {
+  const isProduction = api.env('production');
+
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: isProduction ? ['transform-remove-console'] : [],
+  };
+}
