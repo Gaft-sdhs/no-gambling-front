80539f8d90e1ac7d54dfb0e487b07813eaa0f19d
paulsea27115 <csb070115@gmail.com>
2024-08-13 14:17:10 +0900

    [feat] 충전 버튼 수정

--- src/components/header/HeaderItem.jsx
+++ src/components/header/HeaderItem.jsx
@@ -1,9 +1,23 @@
 import "./css/headerItem.css";
 import { Link } from "react-router-dom";
-const HeaderItem = ({ name, link }) => {
+const HeaderItem = ({ name, action, type }) => {
+  if (type === "NONE") {
+    return (
+      <li className={"headerItem " + type}>
+        <a
+          onClick={() => {
+            action();
+          }}
+        >
+          {name}
+        </a>
+        <span className="underBar"></span>
+      </li>
+    );
+  }
   return (
     <li className="headerItem">
-      <Link to={link}>{name}</Link>
+      <Link to={action}>{name}</Link>
       <span className="underBar"></span>
     </li>
   );
